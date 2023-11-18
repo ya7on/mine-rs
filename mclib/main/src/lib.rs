229e16549ef5b8c9557ea88af2c892e9e30c34b7
ya7on <hello@syoma.website>
2023-11-18 20:37:44 +0300

    Add facade crate for mclib

---
+++ mclib/main/src/lib.rs
@@ -0,0 +1,9 @@
+pub use mclib_macros::MCPacket;
+pub use mclib_protocol::packets::base::MCPacket;
+pub use mclib_protocol::packets::packet_ids::PacketIDS;
+pub use mclib_protocol::types::base::MCType;
+pub use mclib_protocol::types::boolean::MCBoolean;
+pub use mclib_protocol::types::byte::MCByte;
+pub use mclib_protocol::types::ubyte::MCUByte;
+pub use mclib_protocol::types::ushort::MCUShort;
+pub use mclib_protocol::types::varint::MCVarInt;
