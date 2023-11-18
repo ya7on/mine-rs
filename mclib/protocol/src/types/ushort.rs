b0446528870a1b939f035c742120aa317524f79e
ya7on <hello@syoma.website>
2023-11-18 20:36:37 +0300

    Add unsigned short type

---
+++ mclib/protocol/src/types/ushort.rs
@@ -0,0 +1,50 @@
+use crate::types::base::MCType;
+
+#[derive(Debug)]
+pub struct MCUShort(u16);
+
+impl From<u16> for MCUShort {
+    fn from(value: u16) -> Self {
+        Self(value)
+    }
+}
+
+impl Into<u16> for MCUShort {
+    fn into(self) -> u16 {
+        self.0
+    }
+}
+
+impl PartialEq<u16> for MCUShort {
+    fn eq(&self, other: &u16) -> bool {
+        &self.0 == other
+    }
+}
+
+impl MCType for MCUShort {
+    fn pack(&self) -> Vec<u8> {
+        self.0.to_be_bytes().to_vec()
+    }
+
+    fn unpack(src: &mut Vec<u8>) -> Self {
+        Self(u16::from_be_bytes([
+            Self::read_byte(src),
+            Self::read_byte(src),
+        ]))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    #[ignore]
+    fn test_ushort_pack() {
+        unimplemented!()
+    }
+
+    #[test]
+    #[ignore]
+    fn test_ushort_unpack() {
+        unimplemented!()
+    }
+}
