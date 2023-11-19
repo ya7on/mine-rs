bbe3af875e89b963eaf6bf8ef8877a2cbca9c132
ya7on <hello@syoma.website>
2023-11-19 20:17:21 +0300

    Update handshake packet

--- mclib/main/src/lib.rs
+++ mclib/main/src/lib.rs
@@ -1,6 +1,6 @@
 pub use mclib_macros::MCPacket;
 pub use mclib_protocol::packets::base::MCPacket;
-pub use mclib_protocol::packets::packet_ids::PacketIDS;
+pub use mclib_protocol::packets::server::handshake::{Handshake, HandshakeNextState};
 pub use mclib_protocol::types::base::MCType;
 pub use mclib_protocol::types::boolean::MCBoolean;
 pub use mclib_protocol::types::byte::MCByte;
--- mclib/protocol/src/packets/server/handshake.rs
+++ mclib/protocol/src/packets/server/handshake.rs
@@ -5,7 +5,23 @@ use crate::types::ushort::MCUShort;
 use crate::types::varint::MCVarInt;
 use mclib_macros::MCPacket;
 
-#[derive(MCPacket)]
+pub enum HandshakeNextState {
+    Status = 0x01,
+    Login = 0x02,
+    Unknown,
+}
+
+impl From<i32> for HandshakeNextState {
+    fn from(value: i32) -> Self {
+        match value {
+            0x01 => Self::Status,
+            0x02 => Self::Login,
+            _ => Self::Unknown,
+        }
+    }
+}
+
+#[derive(MCPacket, Debug)]
 #[packet(packet_id = 0x00)]
 pub struct Handshake {
     pub protocol_version: MCVarInt,
