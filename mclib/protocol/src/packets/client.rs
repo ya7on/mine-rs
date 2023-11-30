b038d90032c38bb244d58a5fad26b62d714353d1
ya7on <hello@syoma.website>
2023-11-30 22:18:19 +0300

    Added configuration state logic for player login #6 - Send Registry Data (Configuration) - Send Finish Configuration (Clientbound) - Receive Finish Configuration (Serverbound)

--- mclib/protocol/src/packets/client.rs
+++ mclib/protocol/src/packets/client.rs
@@ -1,3 +1,4 @@
+pub mod finish_configuration;
 pub mod login_success;
 pub mod registry_data;
 pub mod status_response;
--- mclib/protocol/src/packets/server.rs
+++ mclib/protocol/src/packets/server.rs
@@ -1,3 +1,4 @@
+pub mod finish_configuration;
 pub mod handshake;
 pub mod login_acknowledged;
 pub mod login_start;
