50183d29d465000216e343eed6dafb84db4a35c6
ya7on <hello@syoma.website>
2023-11-30 22:15:56 +0300

    Export NBT packet field type

--- mclib/protocol/src/types/nbt.rs
+++ mclib/protocol/src/types/nbt.rs
@@ -5,6 +5,18 @@ use std::io::Read;
 #[derive(Debug, Clone)]
 pub struct MCNBT(NBT);
 
+impl From<NBT> for MCNBT {
+    fn from(value: NBT) -> Self {
+        Self(value)
+    }
+}
+
+impl From<MCNBT> for NBT {
+    fn from(value: MCNBT) -> Self {
+        value.0
+    }
+}
+
 impl MCType for MCNBT {
     fn pack(&self) -> Vec<u8> {
         self.0.pack()
