dce18bb894629af312bf87d0274b1713de7b411f
ya7on <hello@syoma.website>
2023-12-06 21:56:07 +0300

    Add hardcoded default light

--- mclib/protocol/src/packets/client/chunk_data_and_update_light.rs
+++ mclib/protocol/src/packets/client/chunk_data_and_update_light.rs
@@ -45,11 +45,11 @@ mod tests {
     use crate::nbt::NBT;
     use crate::packets::base::MCPacket;
     use crate::packets::client::chunk_data_and_update_light::ChunkDataAndUpdateLight;
+    use crate::types::bitset::MCBitSet;
     use crate::types::byte_array::MCByteArray;
     use crate::types::long::MCLong;
 
     #[test]
-    #[ignore]
     fn test_chunk_data() {
         let reference =
             include_bytes!("../../../../../assets/tests/chunk_data_and_update_light.packet");
@@ -96,22 +96,39 @@ mod tests {
             };
             23
         ]);
+        let mut sky_light_mask = MCBitSet::default();
+        sky_light_mask.push(false);
+        sky_light_mask.push(true);
+        sky_light_mask.push(true);
+        let block_light_mask = MCBitSet::default();
+        let mut empty_sky_light_mask = MCBitSet::default();
+        empty_sky_light_mask.push(true);
+        let mut empty_block_light_mask = MCBitSet::default();
+        empty_block_light_mask.push(true);
+        empty_block_light_mask.push(true);
+        empty_block_light_mask.push(true);
+
+        let mut sky = vec![0.into(); 512];
+        sky.extend(vec![(0x0F | (0x0F << 4)).into(); 1536]);
+        let sky_lights = vec![sky, vec![(0x0F | (0x0F << 4)).into(); 2048]];
+
+        let block_lights = vec![];
+
         let chunk_data_and_update_light = ChunkDataAndUpdateLight {
             chunk_x: (-1 as i32).into(),
             chunk_z: 0.into(),
             heightmaps: heightmap.into(),
             data: MCByteArray::new(ChunkData(chunks)),
             block_entities: vec![],
-            sky_light_mask: Default::default(),
-            block_light_mask: Default::default(),
-            empty_sky_light_mask: Default::default(),
-            empty_block_light_mask: Default::default(),
-            sky_lights: vec![],
-            block_lights: vec![],
+            sky_light_mask,
+            block_light_mask,
+            empty_sky_light_mask,
+            empty_block_light_mask,
+            sky_lights,
+            block_lights,
         };
 
         let packed = &chunk_data_and_update_light.pack()[2..];
-        assert_eq!(packed[..2899], reference[..2899]);
-        panic!("{:?}", packed.len());
+        assert_eq!(packed, reference);
     }
 }
--- mclib/protocol/src/types/bitset.rs
+++ mclib/protocol/src/types/bitset.rs
@@ -4,15 +4,27 @@ use crate::utils::TcpUtils;
 use std::io::Read;
 
 #[derive(Debug, Clone, Default)]
-pub struct MCBitSet(Vec<i64>);
+pub struct MCBitSet(Vec<bool>);
+
+impl MCBitSet {
+    pub fn push(&mut self, value: bool) {
+        self.0.push(value)
+    }
+}
 
 impl MCType for MCBitSet {
     fn pack(&self) -> Vec<u8> {
-        let mut result = Vec::new();
-        result.extend(MCVarInt::from(self.0.len() as i32).pack());
-        for long in self.0.iter() {
-            result.extend(long.to_be_bytes());
+        let mut data = Vec::new();
+        for long in self.0.chunks(64) {
+            let mut num = 0_i64;
+            for (bit_index, bit) in long.iter().enumerate() {
+                num |= (*bit as i64) << bit_index;
+            }
+            data.extend(num.to_be_bytes());
         }
+        let mut result = Vec::new();
+        result.extend(MCVarInt::from((data.len() / 8) as i32).pack());
+        result.extend(data);
         result
     }
 
@@ -20,7 +32,7 @@ impl MCType for MCBitSet {
         let len = MCVarInt::unpack(src);
         let mut result = Vec::new();
         for _ in 0..len.into() {
-            result.push(i64::from_be_bytes([
+            let mut num = i64::from_be_bytes([
                 src.read_byte(),
                 src.read_byte(),
                 src.read_byte(),
@@ -29,7 +41,12 @@ impl MCType for MCBitSet {
                 src.read_byte(),
                 src.read_byte(),
                 src.read_byte(),
-            ]));
+            ]);
+
+            for _ in 0..64 {
+                result.push((num & 0b1) == 1);
+                num >>= 1;
+            }
         }
         Self(result)
     }
